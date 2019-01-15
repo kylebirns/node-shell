3eec4175924adc10f2bc0468670481d7a57b81e6
Kyle Birns <kylebirns@gmail.com>
2019-01-15 13:02:46 -0500

    attempt pwd

--- bash.js
+++ bash.js
@@ -1,9 +1,12 @@
+const pwd = require('./pwd');
+
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', (data) => {
     let cmd = data.toString().trim();
+
+    if (pwd.cmd){}
     
     process.stdout.write('You typed: ' + cmd);
 });
 
-const pwd = require('./pwd');
--- pwd.js
+++ pwd.js
@@ -1,17 +1,9 @@
-// module.exports = function () {
-//     if (cmd === 'pwd'){
-//         cmd = process.cwd();
-//     }
-    
-//     process.stdout.write('\nprompt > ');
-// }
-
 module.exports = process.stdin.on('data', (data) => {
     let cmd = data.toString().trim();
     if (cmd === 'pwd'){
         cmd = process.cwd();
     }
-    
-    process.stdout.write('\nprompt > ');
+
     process.stdout.write('You typed: ' + cmd);
+    process.stdout.write('\nprompt > ');
 });
